f363ea46f233982b766665acabfc348746c81347
Justin Hung <justinhung1994@gmail.com>
2025-01-07 23:54:30 +1100

    error page and layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import type { LayoutData } from './$types';
+	import type { Snippet } from 'svelte';
+
+	let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
+
+<h1>Settings</h1>
+
+<div class="submenu">
+	{#each data.sections as section}
+		<a href="/settings/{section.slug}">{section.title}</a>
+	{/each}
+</div>
 
 {@render children()}
