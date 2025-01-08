c65b145e1447e7ba76e43381604555e0d073d855
Justin Hung <justinhung1994@gmail.com>
2025-01-08 15:26:48 +1100

    updated layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import type { Snippet } from 'svelte';
+	import "../app.css";
 
 	let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
-<h1>Settings</h1>
+<div class="flex bg-indigo-300 p-4 justify-between items-center">
+	<div class="flex-none">
+    <img src="https://picsum.photos/64" alt="Logo" height="64" width="64" class="rounded" />
+  </div>
+  <div class="flex-grow mx-4 text-center">
+		<div class="text-3xl font-bold text-indigo-700">
+			SVELTE
+		</div>
+  </div>
+  <div class="flex-initial">
+    <button class="rounded bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4">Login</button>
+  </div>
+</div>
 
-<div class="submenu">
+<!-- <div class="submenu">
 	{#each data.sections as section}
 		<a href="/settings/{section.slug}">{section.title}</a>
 	{/each}
-</div>
+</div> -->
 
 {@render children()}
