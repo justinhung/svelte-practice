129a57d2af77839f98012bda0980fe3a9fffb0e4
Justin Hung <justinhung1994@gmail.com>
2025-01-16 15:47:39 +1100

    removed alert button and added hover transition for cards

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-	function greet() {
-		alert('Welcome to Svelte!');
-	}
 </script>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
 	{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as i}
-		<div class="border rounded p-4">
+		<div class="border transition-all ease-in-out duration-300 rounded p-4 bg-white hover:bg-indigo-100">
 			Card {i}
 		</div>
 	{/each}
-	
 </div>
-<button onclick={greet}>click me</button>
-
-<style>
-	button {
-		font-size: 2em;
-	}
-</style>
