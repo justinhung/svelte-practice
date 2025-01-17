6c060b89d565d95a130b57659e5e2cd8670874b8
Justin Hung <justinhung1994@gmail.com>
2025-01-17 17:37:47 +1100

    minor link change for nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,13 +6,13 @@
 
 <div class="flex bg-indigo-100 space-x-4">
   {#each data.sections as section}
-    <Dropdown href="/settings/{section.slug}">
+    <Dropdown href="/{section.slug}">
 		  <div class="text-indigo-600 font-medium hover:text-indigo-700 pl-4">{section.title}</div>
 
       {#snippet dropDownList()}
         {#each section.links as link}
           <li class="pl-4">
-            <a href="/{link.slug}" class="flex items-center gap-2 p-2 hover:bg-base-200 text-indigo-600 hover:font-medium">
+            <a href="/{section.slug}/{link.slug}" class="flex items-center gap-2 p-2 hover:bg-base-200 text-indigo-600 hover:font-medium">
               {link.title}
             </a>
           </li>
