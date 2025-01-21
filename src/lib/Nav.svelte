cdedc869f73be13b51489d912840df9beaaedbba
Justin Hung <justinhung1994@gmail.com>
2025-01-21 20:12:13 +1100

    dark mode + transition

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,15 +4,15 @@
   export let data
 </script>
 
-<div class="flex bg-indigo-100 space-x-4">
+<div class="flex bg-indigo-100 dark:bg-indigo-900 space-x-4 transition-all ease-in-out duration-300">
   {#each data.sections as section}
     <Dropdown href="/{section.slug}">
-		  <div class="text-indigo-600 font-medium hover:text-indigo-700 pl-4">{section.title}</div>
+		  <div class="text-indigo-600 dark:text-indigo-300 font-medium hover:text-indigo-700 hover:dark:text-indigo-400 pl-4 transition-all ease-in-out duration-300">{section.title}</div>
 
       {#snippet dropDownList()}
         {#each section.links as link}
           <li class="pl-4">
-            <a href="/{section.slug}/{link.slug}" class="flex items-center gap-2 p-2 hover:bg-base-200 text-indigo-600 hover:font-medium">
+            <a href="/{section.slug}/{link.slug}" class="flex items-center gap-2 p-2 hover:bg-base-200 hover:dark:bg-base-800 text-indigo-600 hover:font-medium transition-all ease-in-out duration-300">
               {link.title}
             </a>
           </li>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,20 +11,22 @@
   }
 </script>
 
-<div class={`${isDarkMode ? 'dark' : ''} flex bg-indigo-300 dark:bg-gray-800 p-4 justify-between items-center`}>
-	<div class="flex-none">
-    <img src="/favicon.png" alt="Logo" height="64" width="64" class="rounded" />
+<div class={`${isDarkMode ? 'dark' : ''} h-full`}>
+  <div class="flex bg-indigo-300 dark:bg-indigo-950 p-4 justify-between items-center transition-all ease-in-out duration-300">
+    <div class="flex-none">
+      <img src="/favicon.png" alt="Logo" height="64" width="64" class="rounded" />
+    </div>
+    <div class="flex-grow mx-4 text-center">
+      <div class="text-3xl font-bold text-indigo-800 dark:text-indigo-300 transition-all ease-in-out duration-300">
+        SVELTE
+      </div>
+    </div>
+    <div class="flex-initial">
+      <button class="rounded bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-700 hover:dark:bg-indigo-600 text-white py-2 px-4 transition-all ease-in-out duration-300 " on:click={toggleDarkMode}>Toggle</button>
+    </div>
   </div>
-  <div class="flex-grow mx-4 text-center">
-		<div class="text-3xl font-bold text-indigo-700">
-			SVELTE
-		</div>
-  </div>
-  <div class="flex-initial">
-    <button class="rounded bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4" on:click={toggleDarkMode}>Toggle</button>
-  </div>
+
+  <Nav data={data} />
+
+  {@render children()}
 </div>
-
-<Nav data={data} />
-
-{@render children()}
